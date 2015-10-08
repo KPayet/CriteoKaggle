3e58e06b33422ffb2f8a2ef54787cb0dbb7f9e9e
root <root@ip-172-31-26-143.eu-west-1.compute.internal>
2015-10-08 10:36:24 +0000

    added bash script that copies predictions and models every time if finds one not yet written

---
+++ copyManager.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+COUNTER=0
+while [ $COUNTER -lt 1 ]; do
+    if [ -a ./copyTos3 ]
+        then
+            sudo chmod +x ./copyTos3;
+            sudo ./copyTos3;
+            sudo rm ./copyTos3;
+    fi
+    sleep 10m;
+done
+
