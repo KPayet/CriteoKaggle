5bee8392b61f7214d89849aca68914d1ca5e1de4
Kevin Payet <k_payet@yahoo.fr>
2015-10-27 15:32:55 +0100

    modified the processing of validation set, and changed the hashing part

--- criteo.R
+++ criteo.R
@@ -2,25 +2,38 @@ source("criteo_helper.R")
 
 require(readr)
 
-prepDataTrain = read_delim("train.txt", delim="\t", col_names = F)
-prepDataTrain = prepDataTrain[sample(1:nrow(prepDataTrain), nrow(prepDataTrain), replace=F),]
-prepDataTest = read_delim("test.txt", delim="\t", col_names = F)
+train = read_delim("train.txt", delim="\t", col_names = F)
+train = train[sample(1:nrow(train), nrow(train), replace=F),] # shuffle train set
 
+test = read_delim("test.txt", delim="\t", col_names = F)
+
+# I split the training set into train and validation set right from the beginning, so that the valid set is treated like the test set
+# I don't want any information leakage
+require(caTools)
+split = sample.split(train$X1, SplitRatio = 0.97) # probably makes shuffling redundant
+
+valid = train[!split,]
+train = train[split,]
+
+# scale the numeric features in the three datasets
 require(caret)
-preproc = preProcess(prepDataTrain[,2:14])
-prepDataTrain[,2:14] = predict(preproc, prepDataTrain[,2:14])
+preproc = preProcess(train[,2:14])
+train[,2:14] = predict(preproc, train[,2:14])
 
-names(prepDataTest) = names(prepDataTrain)[2:40]
-prepDataTest[,1:13] = predict(preproc, prepDataTest[,1:13])
+valid[,2:14] = predict(preproc, valid[,2:14])
+
+names(test) = names(train)[2:40]
+test[,1:13] = predict(preproc, test[,1:13])
 
 # make all NA in numeric features -666, to use the missing = -666 flag in xgboost
 
 for(i in 2:14) {
-  prepDataTrain[,i][is.na(prepDataTrain[,i])] = rep(-666, length(prepDataTrain[,i][is.na(prepDataTrain[,i])]))
+  train[,i][is.na(train[,i])] = -666
+  valid[,i][is.na(valid[,i])] = -666
 }
 
 for(i in 1:13) {
-  prepDataTest[,i][is.na(prepDataTest[,i])] = rep(-666, length(prepDataTest[,i][is.na(prepDataTest[,i])]))
+  test[,i][is.na(test[,i])] = -666
 }
 
 # prepare categorical features
@@ -31,26 +44,40 @@ trainLevelsForFeat = list()
 
 # train set
 for(i in 15:40){
-    testCatFeat = prepDataTrain[,i]
+    testCatFeat = train[,i]
     # testCatFeat = as.character(sapply(testCatFeat, FUN = function(x){paste("0x",x,sep="")}))
     # testCatFeat = as.numeric(testCatFeat)
     # testCatFeat[testCatFeat = ""] = -666
     # testCatFeat = as.factor(testCatFeat)
+	if(!( "" %in% levels(testCatFeat) )) { levels(testCatFeat) = c(levels(testCatFeat), "") }
     trainLevelsForFeat[[i]] = levels(testCatFeat) # record which levels are in the train set
-    # prepDataTrain[,i] = testCatFeat
+    # train[,i] = testCatFeat
     print(i)
 }
 rm(list = c("testCatFeat", "i"))
 
+for(i in 15:40){
+  testCatFeat = valid[,i]
+  # testCatFeat = as.character(sapply(testCatFeat, FUN = function(x){paste("0x",x,sep="")}))
+  # testCatFeat = as.numeric(testCatFeat)
+  # testCatFeat[testCatFeat = ""] = -666
+  if(!( "" %in% levels(testCatFeat) )) { levels(testCatFeat) = c(levels(testCatFeat), "") }
+  testCatFeat[ !( testCatFeat %in% trainLevelsForFeat[[i]] ) ] = "" # set levels that were not in the train set to missing
+  testCatFeat = as.factor(testCatFeat)
+  valid[, i] = testCatFeat
+  print(i)
+}
+rm(list = c("testCatFeat", "i"))
+
 for(i in 14:39){
-  testCatFeat = prepDataTest[,i]
+  testCatFeat = test[,i]
   # testCatFeat = as.character(sapply(testCatFeat, FUN = function(x){paste("0x",x,sep="")}))
   # testCatFeat = as.numeric(testCatFeat)
   # testCatFeat[testCatFeat = ""] = -666
   if(!( "" %in% levels(testCatFeat) )) { levels(testCatFeat) = c(levels(testCatFeat), "") }
   testCatFeat[ !( testCatFeat %in% trainLevelsForFeat[[i + 1]] ) ] = "" # set levels that were not in the train set to missing
   testCatFeat = as.factor(testCatFeat)
-  prepDataTest[,i] = testCatFeat
+  test[, i] = testCatFeat
   print(i)
 }
 rm(list = c("testCatFeat", "i"))
@@ -63,12 +90,12 @@ rm(list = c("testCatFeat", "i"))
 # require(xgboost)
 # require(caTools)
 
-# split = sample.split(prepDataTrain$X1, SplitRatio = 0.8)
+# split = sample.split(train$X1, SplitRatio = 0.8)
 
-# train = prepDataTrain[split,]
+# train = train[split,]
 # trainFeats = sparse.model.matrix(~ .-1, data = train[,2:40])
 # train = list(data = trainFeats, label = train$X1)
-# valid = prepDataTrain[!split,]
+# valid = train[!split,]
 # validFeats = sparse.model.matrix(~ .-1, data = valid[,2:40])
 # valid = list(data = validFeats, label = valid$X1)
 
@@ -93,7 +120,7 @@ rm(list = c("testCatFeat", "i"))
 
 # ## Predictions on the test set, and make submission
 
-# testFeats = sparse.model.matrix(~., data = prepDataTest)
+# testFeats = sparse.model.matrix(~., data = test)
 # dtest = xgb.DMatrix(data = testFeats)
 # predTest = predict(xgModel, dtest)
 
@@ -106,16 +133,17 @@ rm(list = c("testCatFeat", "i"))
 
 require(FeatureHashing)
 require(xgboost)
-require(caTools)
 
-split = sample.split(prepDataTrain$X1, SplitRatio = 0.97)
+# split = sample.split(train$X1, SplitRatio = 0.97) # this is done at the beginning in the new version
 
-train = prepDataTrain[split,]
-trainHashFeats = hashed.model.matrix(~ .-1, data = train[,2:40], hash.size = 2^11, transpose = F)
+# train = train[split,]
+trainHashFeats = hashed.model.matrix(~ .-1, data = train[,2:40], hash.size = 2^16, transpose = F)
 train = list(data = trainHashFeats, label = train$X1)
-valid = prepDataTrain[!split,]
-validHashFeats = hashed.model.matrix(~ .-1, data = valid[,2:40], hash.size = 2^11, transpose = F)
+# valid = train[!split,]
+validHashFeats = hashed.model.matrix(~ .-1, data = valid[,2:40], hash.size = 2^16, transpose = F)
 valid = list(data = validHashFeats, label = valid$X1)
+
+rm(list = c("trainHashFeats", "validHashFeats"))
 
 # saveRDS(train, "trainListHashed2048.rds")
 # saveRDS(valid, "validListHashed2048.rds")
@@ -126,12 +154,12 @@ dtrain = xgb.DMatrix(data = train$data, label = train$label)
 dvalid = xgb.DMatrix(data = valid$data, label = valid$label)
 
 # do cross validation using xgb.cv to find best parameters
-cvResults = grid.Search(dvalid, .md = seq(4, 8, 2), .gamma = seq(1.5, 3.5, 0.5), 
-								.minChildWeight = c(1, 5, 10), .nround = c(100, 200, 500),
+cvResults = grid.Search(dvalid, .md = seq(6, 14, 2), .gamma = seq(1.5, 9.5, 0.5), 
+								.minChildWeight = c(1, 5, 10), .nround = c(100, 200, 500, 1000),
 								nFolds = 5)
 
 # testFeats = readRDS("testListHashed2048.rds")
-testFeats = hashed.model.matrix(~ .-1, data = prepDataTest, hash.size = 2^11, transpose = F)
+testFeats = hashed.model.matrix(~ .-1, data = test, hash.size = 2^16, transpose = F)
 # saveRDS(testFeats, "testListHashed2048.rds")
 dtest = xgb.DMatrix(data = testFeats)
 
