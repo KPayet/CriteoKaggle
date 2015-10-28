15ecb384f6c7f1c534e3939434d76750edd6e1ba
Kevin Payet <k_payet@yahoo.fr>
2015-10-28 14:17:10 +0100

    added python spark scripts

---
+++ criteoSpark.py
@@ -0,0 +1,119 @@
+from helperFunctions import *
+
+import sys
+import getopt
+import os
+from pyspark import SparkContext
+
+sc = SparkContext(appName="CriteoKaggle")
+
+# Load data
+rawData = (sc.textFile("./dac_sample.txt", 10) # change value here for number of partitions
+		     .map(lambda x: x.replace('\t', ',')))
+
+
+weights = [.97, .03]
+seed = 1234
+
+rawTrainData, rawValidationData = rawData.randomSplit(weights, seed)
+# Cache the data for performance
+rawTrainData.cache()
+rawValidationData.cache()
+
+# Make sure that label is simply Id for the test set, i.e. the line of the observation + 59999999
+rawTestSet = (sc.textFile("test.txt", 10) # change value here for number of partitions
+                .map(lambda x: x.replace('\t', ','))
+				.zipWithIndex()
+				.map(lambda t: str( t[1] + 60000000 ) + "," + t[0] ))
+
+
+# Create the dictionary for one-hot encoding from the train set and compute number of features in train set
+
+trainFeatures = rawTrainData.map(parse)
+
+OHEDict = makeOHEDict(trainFeatures)
+numTrainOHEFeatures = len(OHEDict.keys())
+
+# One-Hot encoding of data in train, valid and test sets
+
+OHETrainData = rawTrainData.map(lambda point: parse2OHE(point, OHEDict, numTrainOHEFeatures))
+OHETrainData.cache()
+
+OHEValidationData = rawValidationData.map(lambda point: parse2OHE(point, OHEDict, numTrainOHEFeatures))
+OHEValidationData.cache()
+
+# in the test set, the label of the LabeledPoint will simply be the observation ID required in Kaggle submission
+testOHE = rawTestSet.map(lambda point: parse2OHE(point, OHEDict, numTrainOHEFeatures))
+testOHE.cache()
+
+# Logistic regression on OHE features
+
+from pyspark.mllib.classification import LogisticRegressionWithSGD
+
+numIters = 50
+stepSize = 10.
+regParam = 1e-6
+
+model0 = LogisticRegressionWithSGD.train(OHETrainData, numIters, stepSize, 1.0, None, regParam, 'l2', True)
+
+# Model evaluation
+
+validLogLossModel0 = evaluateModel(model0, OHEValidationData)
+
+# Predict on the test set
+
+testPredictions = (testOHE.map(lambda p: (p.label, getCTRProb(p.features, bestModel.weights, bestModel.intercept))) # in test label is simply observation Id, needed for Kaggle submission.
+                          .map(lambda t: str(t[0]) + "," + str(t[1]))
+                          .coalesce(1)
+                          .saveAsTextFile("predictions.csv"))
+
+
+ # Using feature hashing instead of OHE
+
+hashedTrainData = rawTrainData.map(lambda point: createHashedPoint(point, 2**15))
+hashedTrainData.cache()
+hashedValidationData = rawValidationData.map(lambda point: createHashedPoint(point, 2**15))
+hashedValidationData.cache()
+
+
+# retrain a logistic regression, but do some grid search before
+
+# Initialize variables using values from initial model training
+bestModel = None
+bestLogLoss = float("inf")
+
+stepSizes = [1, 5, 10, 20]
+regParams = [0.000001, 0.0001, 0.001, 0.01]
+
+for stepSize in stepSizes:
+    for regParam in regParams:
+
+        model = (LogisticRegressionWithSGD
+                 .train(hashedTrainData, 500, stepSize, regParam=regParam, regType='l2',
+                        intercept=True))
+
+        logLossVa =  (hashedValidationData.map(lambda p: (p.label, getCTRProb(p.features, model.weights, model.intercept)))
+                                 .map(lambda p: computeLogLoss(p[1], p[0]))
+                                 .reduce(lambda a,b: a+b))/hashedValidationData.count()
+
+#        logLossVa = evaluateModel(model, hashedValidationData)
+
+        if (logLossVa < bestLogLoss):
+            bestModel = model
+            bestLogLoss = logLossVa
+
+# predict on test set
+
+testHashed = rawTestSet.map(lambda point: createHashedPoint(point, 2**15))
+
+testPredictions = (testHashed.map(lambda p: (p.label, getCTRProb(p.features, bestModel.weights, bestModel.intercept))) # in test label is simply observation Id, needed for Kaggle submission.
+                             .map(lambda t: str(t[0]) + "," + str(t[1]))
+                             .coalesce(1)
+                             .saveAsTextFile("predictions.csv"))
+
+
+
+
+
+
+
---
+++ helperFunctions.py
@@ -0,0 +1,146 @@
+"""
+This file contains the helper functions for preparing the data, training the model ....
+"""
+
+"""
+Function used to one-hot encode categorical features of the dataset
+i.e. turn the categorical features into binary features (K binary features for a K-level nominal feature)
+The produced encoding is stored in a SparseVector for space efficiency
+"""
+def oneHotEncode(features, featDict, numFeatures):
+
+    N = [];
+
+    for feat in features:
+        if featDict.has_key(feat):
+            N.append(featDict[feat])
+    N = sorted(N)
+
+    return SparseVector(numOHEFeats, N, [1.]*len(N))
+
+
+"""
+This function creates a dictionary used for one-hot encoding the data
+"""
+def makeOHEDict(input):
+
+    distinctFeats = (input
+                       .flatMap(lambda s: s)
+                       .distinct())
+    dic = (distinctFeats
+                .zipWithIndex()
+                .collectAsMap())
+    return dic
+
+"""
+Converts the comma separated input observations into a list of (feature ID, value) tuples
+feature ID = [0, numFeatures - 1]
+The function returns only the features, not the label (which is the first value of the string).
+"""
+def parse(point):
+
+    i=0
+    pList = []
+    for feat in point.split(',')[1:]:
+        pList.append((i, feat))
+        i=i+1
+
+    return pList
+
+"""
+Transforms the raw string format of observations into a LabeledPoint.
+For test data, the string is modified so that the first number is the observation ID required for Kaggle submission
+"""
+from pyspark.mllib.regression import LabeledPoint
+def parse2OHE(point, dict, numFeatures):
+
+    label = point.split(',')[0]
+    feats = parse(point)
+
+    encodedFeats = oneHotEncode(feats, dict, numFeatures)
+
+    return LabeledPoint(label, encodedFeats)
+
+from math import log
+
+"""
+This function computes the logloss
+"""
+def computeLogLoss(prob, y):
+
+    epsilon = 10e-12
+
+    if prob == 0:
+        prob = prob + epsilon
+    elif prob == 1:
+        prob = prob - epsilon
+
+    if y == 1:
+        loss = -log(prob)
+    else:
+        loss = -log(1-prob)
+
+    return loss
+
+"""
+This function computes the probability of CTR given observed features X, and weights for a logistic regression model W, and the W0
+"""
+from math import exp
+def getCTRProb(X, W, W0):
+
+    rawPrediction = W0 + X.dot(W)
+
+    # Bound the raw prediction value
+    rawPrediction = min(rawPrediction, 20)
+    rawPrediction = max(rawPrediction, -20)
+    return 1/(1+exp(-rawPrediction))
+
+"""
+Computes the log loss on the a data set to evaluate the model
+"""
+
+def evaluateModel(model, data):
+
+    logLoss = (data.map(lambda p: (p.label, getCTRProb(p.features, model0.weights, model0.intercept)))
+                 .map(lambda p: computeLogLoss(p[1],p[0]))
+                 .reduce(lambda a,b: a+b))/data.count()
+
+    return logLoss
+
+"""
+Hash function used for the feature hashing trick
+"""
+from collections import defaultdict
+import hashlib
+
+def hashThis(nBuckets = 2**16, features):
+
+    mapping = {}
+
+    for i, cat in features:
+        featureString = cat + str(i)
+        mapping[featureString] = int(int(hashlib.md5(featureString).hexdigest(), 16) % nBuckets)
+
+    sparseFeatures = defaultdict(float)
+    for bucket in mapping.values():
+        sparseFeatures[bucket] += 1.0
+    return dict(sparseFeatures)
+
+"""
+This function uses the parse function defined above, and the hash function
+to create for each observation a LabeledPoint, with label, and where features is a
+SparseVector containing the hashed features.
+"""
+def createHashedPoint(point, nBuckets):
+
+    splitPoint = point.split(',')
+    label = splitPoint[0]
+    feats = parse(point)
+
+    hashedFeats = hashThis(nBuckets, feats)
+
+    nonZeroIndices = sorted([key for key in hashedFeats])
+    nonZeroValues = [hashedFeats[key] for key in nonZeroIndices]
+
+    return LabeledPoint(label, SparseVector(nBuckets, nonZeroIndices, nonZeroValues))
+
