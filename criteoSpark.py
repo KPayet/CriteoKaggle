a320d8928fbee3d927a3c93a6eb19cdba26d1608
Kevin Payet <k_payet@yahoo.fr>
2015-10-29 16:31:46 +0100

    commented out whole OHE processing

--- criteoSpark.py
+++ criteoSpark.py
@@ -27,46 +27,48 @@ rawTestSet = (sc.textFile("test.txt", 10) # change value here for number of part
 				.map(lambda t: str( t[1] + 60000000 ) + "," + t[0] ))
 
 
+# Processing using One-hot encoding
+
 # Create the dictionary for one-hot encoding from the train set and compute number of features in train set
 
-trainFeatures = rawTrainData.map(parse)
-
-OHEDict = makeOHEDict(trainFeatures)
-numTrainOHEFeatures = len(OHEDict.keys())
-
-# One-Hot encoding of data in train, valid and test sets
-
-OHETrainData = rawTrainData.map(lambda point: parse2OHE(point, OHEDict, numTrainOHEFeatures))
-OHETrainData.cache()
-
-OHEValidationData = rawValidationData.map(lambda point: parse2OHE(point, OHEDict, numTrainOHEFeatures))
-OHEValidationData.cache()
-
-# in the test set, the label of the LabeledPoint will simply be the observation ID required in Kaggle submission
-testOHE = rawTestSet.map(lambda point: parse2OHE(point, OHEDict, numTrainOHEFeatures))
-testOHE.cache()
-
-# Logistic regression on OHE features
-
-from pyspark.mllib.classification import LogisticRegressionWithSGD
-
-numIters = 50
-stepSize = 10.
-regParam = 1e-6
-
-model0 = LogisticRegressionWithSGD.train(OHETrainData, numIters, stepSize, 1.0, None, regParam, 'l2', True)
-
-# Model evaluation
-
-validLogLossModel0 = evaluateModel(model0, OHEValidationData)
-
-# Predict on the test set
-
-testPredictions = (testOHE.map(lambda p: (p.label, getCTRProb(p.features, model0.weights, model0.intercept))) # in test label is simply observation Id, needed for Kaggle submission.
-                          .map(lambda t: str(int(t[0])) + "," + str(t[1]))
-                          .coalesce(1)
-                          .saveAsTextFile("./predictions.csv"))
-
+#trainFeatures = rawTrainData.map(parse)
+#
+#OHEDict = makeOHEDict(trainFeatures)
+#numTrainOHEFeatures = len(OHEDict.keys())
+#
+## One-Hot encoding of data in train, valid and test sets
+#
+#OHETrainData = rawTrainData.map(lambda point: parse2OHE(point, OHEDict, numTrainOHEFeatures))
+#OHETrainData.cache()
+#
+#OHEValidationData = rawValidationData.map(lambda point: parse2OHE(point, OHEDict, numTrainOHEFeatures))
+#OHEValidationData.cache()
+#
+## in the test set, the label of the LabeledPoint will simply be the observation ID required in Kaggle submission
+#testOHE = rawTestSet.map(lambda point: parse2OHE(point, OHEDict, numTrainOHEFeatures))
+#testOHE.cache()
+#
+## Logistic regression on OHE features
+#
+#from pyspark.mllib.classification import LogisticRegressionWithSGD
+#
+#numIters = 50
+#stepSize = 10.
+#regParam = 1e-6
+#
+#model0 = LogisticRegressionWithSGD.train(OHETrainData, numIters, stepSize, 1.0, None, regParam, 'l2', True)
+#
+## Model evaluation
+#
+#validLogLossModel0 = evaluateModel(model0, OHEValidationData)
+#
+## Predict on the test set
+#
+#testPredictions = (testOHE.map(lambda p: (p.label, getCTRProb(p.features, model0.weights, model0.intercept))) # in test label is simply observation Id, needed for Kaggle submission.
+#                          .map(lambda t: str(int(t[0])) + "," + str(t[1]))
+#                          .coalesce(1)
+#                          .saveAsTextFile("./predictions.csv"))
+#
 
  # Using feature hashing instead of OHE
 
